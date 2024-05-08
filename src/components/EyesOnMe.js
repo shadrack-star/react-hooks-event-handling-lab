179878cf9c2f8d183a274aa5751b62dee90669a2
shadrack-star <shaddkiplangat98@gmail.com>
2024-05-08 23:39:57 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function focus() {
+        console.log("Good!");
+    }
+
+    function Blur() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button onFocus={focus} onBlur={Blur}>
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+function Keypad() {
+
+    function handlePasswordChange() {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
-    )
+
+        <input type="password" onChange={handlePasswordChange} />
+
+    );
 }
 
 export default Keypad;
